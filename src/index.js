6f861312a28bd950e9a949b93ad136354f01ad42
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-07-04 19:55:57 -0500

    add linter folder

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Background from './components/Background';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-      <Background />
+      <Background />    
       <Navbar />
       <App />   
   </>  
