8f50eff57dc7beb4c4bd686c3f119f3e43dd6057
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-07-04 23:12:11 -0500

    peer review

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './scss/index.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Background from './components/Background';
